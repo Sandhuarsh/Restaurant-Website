1fa61bf8dc3ad3607fb705159c6b9e5a19235b9c
Arshpreet <Sandhuarshpreet123@gmail.com>
2023-07-26 21:17:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Restaurant from './Components/Restaurant';
+// import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+    <Restaurant/>
+    </div>
+  )
+}
+
+export default App;
